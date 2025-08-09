d0324452f6ccb9bbcce70c572db28b9cf1f6a2a8
Nico Serrano <nnicosp@hotmail.com>
2025-08-09 11:49:13 -0500

    fix: use nest mapped types

--- apps/backend/src/circuits/dto/update-circuit.dto.ts
+++ apps/backend/src/circuits/dto/update-circuit.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateCircuitDto } from './create-circuit.dto';
 
 export class UpdateCircuitDto extends PartialType(CreateCircuitDto) {}
