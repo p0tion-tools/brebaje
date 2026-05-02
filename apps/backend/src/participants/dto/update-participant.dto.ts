fc36c761da0c5635a514969b84de2f94edbe5441
AgustinBadi <librenotgratis@tuta.io>
2026-05-02 00:04:43 -0400

    fix(backend): fix UpdateParticipantDto

--- apps/backend/src/participants/dto/update-participant.dto.ts
+++ apps/backend/src/participants/dto/update-participant.dto.ts
@@ -1,25 +1,8 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsObject, IsOptional } from 'class-validator';
-import { ParticipantStatus, ParticipantContributionStep } from 'src/types/enums';
+import { IsObject, IsOptional } from 'class-validator';
 import { TemporaryParticipantContributionData } from 'src/types';
 
 export class UpdateParticipantDto {
-  @ApiPropertyOptional({
-    enum: ParticipantStatus,
-    description: 'The updated status of the participant.',
-  })
-  @IsOptional()
-  @IsEnum(ParticipantStatus)
-  status?: ParticipantStatus;
-
-  @ApiPropertyOptional({
-    enum: ParticipantContributionStep,
-    description: 'The updated contribution step of the participant.',
-  })
-  @IsOptional()
-  @IsEnum(ParticipantContributionStep)
-  contributionStep?: ParticipantContributionStep;
-
   @ApiPropertyOptional({
     description: 'Temporary contribution data used for resuming an interrupted contribution.',
   })
